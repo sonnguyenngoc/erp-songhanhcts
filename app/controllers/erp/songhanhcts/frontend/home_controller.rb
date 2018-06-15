7e808c6664501a3be78eb4fec4ea7ca4a3813918
Ton Hung Nguyen <tonhungstar@gmail.com>
2018-06-15 18:47:29 +0000

    sort by custom order

--- app/controllers/erp/songhanhcts/frontend/home_controller.rb
+++ app/controllers/erp/songhanhcts/frontend/home_controller.rb
@@ -3,7 +3,7 @@ module Erp
     module Frontend
       class HomeController < Erp::Frontend::FrontendController
         def index
-          @sliders = Erp::Banners::Banner.get_home_sliders
+          @sliders = Erp::Banners::Banner.get_home_sliders.order('erp_banners_banners.custom_order asc')
           @newest_blogs = Erp::Articles::Article.newest_articles(4)
           @testimonials = Erp::Testimonials::Testimonial.get_testimonials
         end
