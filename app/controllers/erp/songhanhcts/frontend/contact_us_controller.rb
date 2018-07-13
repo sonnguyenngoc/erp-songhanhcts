7975a160dc6d646b55a26b71dd1cefcd6589f879
Ton Hung Nguyen <tonhungstar@gmail.com>
2018-07-13 16:06:35 +0000

    cap nhat chuc nang cho trang lien he

--- app/controllers/erp/songhanhcts/frontend/contact_us_controller.rb
+++ app/controllers/erp/songhanhcts/frontend/contact_us_controller.rb
@@ -5,28 +5,32 @@ module Erp
         def index
           @company_info = Erp::Contacts::Contact.get_main_contact
           
-          #if params[:contact].present?
-          #  @contact = Erp::Contacts::Contact.new(contact_params)
-          #  @contact.contact_type = Erp::Contacts::Contact::TYPE_PERSON
-          #  if @contact.save and params[:msg].present?
-          #    @msg = Erp::Contacts::Message.new(message_params)
-          #    @msg.contact_id = @contact.id
-          #    # @todo get email receive contact
-          #    @msg.to_contact_id = Erp::Contacts::Contact.first.id
-          #    respond_to do |format|
-          #      if @msg.save
-          #        Erp::Contacts::ContactMailer.sending_email_contact(@msg).deliver_now
-          #        format.html {
-          #          redirect_back(fallback_location: @contact, notice: 'Yêu cầu đã gửi thành công.\n Chúng tôi sẽ liên hệ cho bạn trong thời gian sớm nhất.')
-          #        }
-          #      end
-          #    end
-          #  else
-          #    redirect_back(fallback_location: @contact, flash: { error: 'Không thể gửi đi do một số trường đang bị bỏ trống.' })
-          #  end
-          #end
+          if params[:contact].present?
+            @contact = Erp::Contacts::Contact.new(contact_params)
+            @contact.contact_type = Erp::Contacts::Contact::TYPE_PERSON
+            if @contact.save and params[:msg].present?
+              @msg = Erp::Contacts::Message.new(message_params)
+              @msg.contact_id = @contact.id
+              # @todo get email receive contact
+              @msg.to_contact_id = Erp::Contacts::Contact.first.id
+              if @msg.save
+                #Erp::Contacts::ContactMailer.sending_email_contact(@msg).deliver_now
+                  render :json => {status: 'success', notice: 'Yêu cầu của bạn đã được gửi thành công.'}
+              end
+            else
+              render :json => {status: 'error', notice: 'Lỗi! Yêu cầu của bạn chưa được gửi. Vui lòng kiểm tra và thử lại.'}
+            end
+          end
         end
         
+        private
+          def contact_params
+            params.fetch(:contact, {}).permit(:name, :email, :phone)
+          end
+          
+          def message_params
+            params.fetch(:msg, {}).permit(:message)
+          end
       end
     end
   end
---
+++ public/songhanhcts/js/main.js
@@ -0,0 +1,48 @@
+$(function() {
+    // Get the form.
+    var form = $('#ajax-contact-form');
+
+    // Get the messages div.
+    var formMessages = $('.form-notice');
+
+    // Set up an event listener for the contact form.
+	$(form).submit(function(event) {
+		event.preventDefault(); // Stop the browser from submitting the form (prevent default action).
+		var post_url = $(this).attr("action"); //get form action url
+		var request_method = $(this).attr("method"); //get form GET/POST method
+		var form_data = $(this).serialize(); //Encode form elements for submission
+		
+		$.ajax({
+			url : post_url,
+			type: request_method,
+			data : form_data
+		}).done(function(data){ //
+			if (data.status == 'success') {
+				if ($(formMessages).hasClass("error")) {
+					$(formMessages).removeClass('error');
+					$(formMessages).addClass('success');
+				}
+				
+				$('form input, form textarea').val(''); // Clear the form.
+                
+				$(formMessages).html(
+					'<div class="alert alert-success fade in alert-dismissible">' +
+						'<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">&times;</a>' +
+						data.notice +
+					'</div>'
+				);
+			} else if (data.status == 'error') {
+				if ($(formMessages).hasClass("success")) {
+					$(formMessages).removeClass('success');
+					$(formMessages).addClass('error');
+				}
+				$(formMessages).html(
+					'<div class="alert alert-danger fade in alert-dismissible">' +
+						'<a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">&times;</a>' +
+						data.notice +
+					'</div>'
+				);
+			}
+		});
+	});
+});
