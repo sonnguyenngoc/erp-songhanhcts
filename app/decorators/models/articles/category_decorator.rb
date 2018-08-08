e06bdaf2fc29326011b4728f356add8191a2002e
Ton Hung Nguyen <tonhungstar@gmail.com>
2018-08-08 09:12:37 +0000

    update backend sidebar menu

--- app/decorators/models/articles/category_decorator.rb
+++ app/decorators/models/articles/category_decorator.rb
@@ -6,11 +6,11 @@ Erp::Articles::Category.class_eval do
   # get alias
   def self.get_alias_options()
     [
-      {text: I18n.t('about_us'),value: self::ALIAS_ABOUT_US},
-      {text: I18n.t('identity'),value: self::ALIAS_IDENTITY},
-      {text: I18n.t('service'),value: self::ALIAS_SERVICE},
       {text: I18n.t('blog'),value: self::ALIAS_BLOG},
       {text: I18n.t('career'),value: self::ALIAS_CAREER},
+      {text: I18n.t('identity'),value: self::ALIAS_IDENTITY},
+      {text: I18n.t('service'),value: self::ALIAS_SERVICE},
+      {text: I18n.t('about_us'),value: self::ALIAS_ABOUT_US},
       {text: I18n.t('home_about'),value: self::ALIAS_HOME_ABOUT},
       {text: I18n.t('cooperative_customers'),value: self::ALIAS_COOPERATIVE_CUSTOMERS},
       {text: I18n.t('company_structure'),value: self::ALIAS_COMPANY_STRUCTURE}
